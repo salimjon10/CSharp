8768946897769d9024e00e01611488bb1cb33fe6
salimjon10 <salimjonrahimov10@mail.ru>
2023-02-14 16:11:12 +0700

    Закончил 1 лабу

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿/************************
+ * Рахимов С.О          *
+ * Дата : 13.02.2023    *
+ * Базовые понятия С#   *
+ * *********************/
+
+
+
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число a:");
+            int.TryParse(Console.ReadLine(), out int Number);
+
+            Console.Write("Введите число n:");
+            int.TryParse(Console.ReadLine(), out int Degree);
+
+            int Result = 1;
+
+            for (int Step = 0; Step < Degree; ++Step)
+            {
+                Result *= Number;
+            }
+
+            Console.WriteLine($"{Number}^{Degree} = {Result}");
+
+        }
+    }
+}
